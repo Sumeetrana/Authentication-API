8361d7b11ce9868fd4e8ed0c9fc9ae60c0290ee9
sumeet.rana <sumeetsinh28@gmail.com>
2022-06-29 11:18:26 +0530

    cors policy configured

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 import dotenv from 'dotenv';
 dotenv.config()
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT;
+
+// CORS policy
+app.use(cors());
 
 app.listen(port, () => {
     console.log("Server listening...");
